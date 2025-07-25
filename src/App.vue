b76c28c4fc59a8d4c2c68057b1379fdfd0039477
Damian Jacob <daamian91@gmail.com>
2025-07-25 15:37:19 +0200

    added small adjustment to UploadForm

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="container mx-auto globalPadding">
     <app-header @upload="showUpload" />
-    <div v-if="uploadActive" class="bg-black/70 w-screen h-screen absolute z-50 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-      <upload-form @close-upload="hideUpload" class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"/>
-    </div>
+    <transition name="fade-bg">
+      <div v-if="uploadActive"
+        class="bg-black/70 w-screen h-screen absolute z-50 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <transition name="slide-fade">
+          <upload-form v-if="uploadActive" @submitted="hideUploadForm" @close-upload="hideUpload"
+            class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" />
+        </transition>
+      </div>
+    </transition>
     <router-view />
-    <global-player/>
+    <global-player />
   </div>
 </template>
 
@@ -14,7 +20,7 @@
 import AppHeader from "./components/AppHeader.vue";
 import GlobalPlayer from "./components/GlobalPlayer.vue";
 import UploadForm from "./components/UploadForm.vue";
-import {ref} from "vue";
+import { ref } from "vue";
 
 const uploadActive = ref(false)
 
@@ -27,4 +33,34 @@ const hideUpload = () => {
   document.body.style.overflow = '';
   uploadActive.value = false
 }
+
+const hideUploadForm = () => {
+  setTimeout(() => {
+    uploadActive.value = false;
+    document.body.style.overflow = '';
+  }, 3000);
+}
 </script>
+
+<style scoped>
+.fade-bg-enter-active,
+.fade-bg-leave-active {
+  transition: opacity 0.3s ease;
+}
+
+.fade-bg-enter-from,
+.fade-bg-leave-to {
+  opacity: 0;
+}
+
+.slide-fade-enter-active,
+.slide-fade-leave-active {
+  transition: all 0.3s ease;
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
+}
+</style>
--- src/components/UploadForm.vue
+++ src/components/UploadForm.vue
@@ -1,42 +1,62 @@
 <template>
   <form @submit.prevent="handleUpload"
-    class="lg:min-w-[1000px] max-lg:w-full mx-auto p-4 bg-white dark:bg-gray-800 rounded-lg shadow">
-    <h2 class="text-xl font-bold mb-4">Upload your track</h2>
-    <div class="lg:flex items-start justify-between">
-      <div class="lg:w-1/2">
-        <label class="block mb-2 font-semibold text-sm">Title</label>
-        <input v-model="title" type="text" class="input" required />
+    class="lg:min-w-[1000px] max-lg:w-[80%] mx-auto p-4 bg-white dark:bg-gray-800 rounded-lg shadow">
+    <div v-if="!isTrackUploaded">
+      <h2 class="text-xl font-bold mb-4">Upload your track</h2>
+      <div class="lg:flex gap-4 items-start justify-between">
+        <div class="lg:w-1/2">
+          <label class="block mb-2 font-semibold text-sm">Title</label>
+          <input v-model="title" type="text" class="input" required />
 
-        <label class="block mt-4 mb-2 font-semibold text-sm">Description</label>
-        <textarea v-model="description" rows="3" class="input" />
+          <label class="block mt-4 mb-2 font-semibold text-sm">Description</label>
+          <textarea v-model="description" rows="3" class="input" />
 
-        <label class="block mt-4 mb-2 font-semibold text-sm">Tags</label>
-        <input v-model="tags" type="text" class="input" required />
-        <p>{{ tags }}</p>
-      </div>
-      <div class="lg:w-1/2">
-        <label class="block mb-2 font-semibold text-sm">Audio file (.mp3/.wav)</label>
-        <input type="file" @change="handleAudioFile" accept="audio/*" class="input" required />
-        <div v-if="audioFile" class="text-xs mt-1 text-gray-500">{{ audioFile.name }} ({{ formatSize(audioFile.size) }})
+          <label class="block mt-4 mb-2 font-semibold text-sm">Tags</label>
+          <input v-model="tags" type="text" class="input" required />
         </div>
+        <div class="lg:w-1/2">
+          <label class="block mb-2 font-semibold text-sm">Audio file (.mp3/.wav)</label>
+          <div class="relative">
+            <label for="file-upload" class="cursor-pointer inline-flex gap-2">
+              <arrow-up-tray-icon class="heroicon--big" />
+              <span>Wybierz plik</span>
 
-        <label class="block mt-4 mb-2 font-semibold text-sm">Cover</label>
-        <input type="file" @change="handleCoverFile" accept="image/*" class="input" />
-        <img v-if="coverPreview" :src="coverPreview" class="w-32 h-32 object-cover mt-2 rounded" />
+            </label>
+            <input id="file-upload" type="file" @change="handleAudioFile" accept="audio/*" class="hidden" required />
+            <div v-if="audioFile" class="text-xs mt-1 text-gray-500 absolute top-0 left-32">{{ audioFile.name }} ({{
+              formatSize(audioFile.size) }})
+            </div>
+          </div>
+
+          <label class="block mt-4 mb-2 font-semibold text-sm">Cover</label>
+          <label for="cover-upload" class="cursor-pointer inline-flex gap-2">
+            <arrow-up-tray-icon class="heroicon--big" />
+            <span>Wybierz plik</span>
+
+          </label>
+          <input id="cover-upload" type="file" @change="handleCoverFile" accept="image/*" class="hidden" />
+          <img v-if="coverPreview" :src="coverPreview"
+            class="w-32 h-32 object-cover mt-2 rounded absolute max-lg:bottom-4 max-lg:left-36 max-sm:h-16 max-sm:w-16 max-sm:bottom-16" />
+        </div>
+      </div>
+      <div class="flex items-center justify-between">
+        <action-button type="submit" class="mt-6">Upload</action-button>
+        <action-button @click="closeUpload" type="button" class="mt-6">Close</action-button>
       </div>
     </div>
-    <div class="flex items-center justify-between">
-      <action-button type="submit" class="mt-6">Upload</action-button>
-      <action-button @click="closeUpload" type="button" class="mt-6">Close</action-button>
+    <div v-else>
+      <h2 class="text-xl font-bold">Twój plik został załadowany pomyślnie.</h2>
     </div>
-
   </form>
+
+
 </template>
 
 <script setup>
 import { ref } from 'vue'
 import { useUserStore } from "../stores/userStore.js";
 import ActionButton from './ActionButton.vue'
+import { ArrowUpTrayIcon } from '@heroicons/vue/24/outline';
 
 const title = ref('')
 const description = ref('')
@@ -46,6 +66,7 @@ const coverFile = ref(null)
 const coverPreview = ref(null)
 
 const userStore = useUserStore()
+const isTrackUploaded = ref(false)
 
 const handleAudioFile = (e) => {
   audioFile.value = e.target.files[0]
@@ -68,6 +89,7 @@ const handleUpload = () => {
   const newTrack = {
     id: Date.now(),
     title: title.value,
+    artist: userStore.userData.name,
     description: description.value,
     tags: tags.value.split(',').map(t => t.trim()).filter(Boolean),
     audio: audioFile.value,
@@ -83,15 +105,19 @@ const handleUpload = () => {
   audioFile.value = null
   coverFile.value = null
   coverPreview.value = null
+
+  isTrackUploaded.value = true
+  emit('submitted');
 }
-const emit = defineEmits(['closeUpload'])
+const emit = defineEmits(['closeUpload', 'submitted'])
 const closeUpload = () => {
-  emit('closeUpload')
+  emit('closeUpload');
 }
+
 </script>
 
 <style scoped>
 .input {
-  @apply w-full px-3 py-2 border rounded bg-white dark:bg-gray-700 dark:text-white;
+  @apply w-full px-2 border rounded bg-white dark:bg-gray-700 dark:text-white outline-none;
 }
 </style>
